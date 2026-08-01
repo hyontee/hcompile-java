1ad1261a6ff01de80a1070a9898b6a845eabaa6a
root <root@hyonte.com>
2026-08-01 12:33:50 +0000

    JNI build 2026-08-01 12:33:50

---
+++ app/src/main/java/ru/matrp/bonus/GTASA.java
@@ -0,0 +1,12 @@
+package ru.matrp.bonus;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GTASA extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
